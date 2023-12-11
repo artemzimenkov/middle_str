3ecde1796fc2c35cd44317b119622d3b5e827965
artemzimenkov <143996814+artemzimenkov@users.noreply.github.com>
2023-12-11 10:10:20 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -2,12 +2,12 @@
 #include <string>
 using namespace std;
 
-#ifndef GHJ_H_INCLUDED
-#define GHJ_H_INCLUDED
+#ifndef MIDDLE_STR_H_INCLUDED
+#define MIDDLE_STR_H_INCLUDED
 
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
 bool itc_compare(string s1, string s2);
 
-#endif // GHJ_H_INCLUDED
+#endif // MIDDLE_STR_H_INCLUDED
