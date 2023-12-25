ad6a0321b4d050a25a1b65441c7da7feb0690589
artemzimenkov <143996814+artemzimenkov@users.noreply.github.com>
2023-12-25 12:05:54 +0300

    Update middle_str(1-5).cpp

--- middle_str(1-5).cpp
+++ middle_str(1-5).cpp
@@ -48,6 +48,19 @@ bool itc_compare(string s1, string s2){
     return true;
 }
 
+string pol(string str, int nach, int kon){
+    string otv;
+    if (nach > kon)
+        return str;
+    if (kon > itc_len(str)- 1)
+        kon = itc_len(str) - 1;
+    for (int i = nach; i <= kon; i++){
+        otv += str[i];
+    }
+    return otv;
+
+}
+
 bool slo(string sl){
     for(int i = 0; i < itc_len(sl); i++)
     if (!(sl[i] >= 'a' && sl[i] <= 'z'))
@@ -58,16 +71,18 @@ bool slo(string sl){
 int itc_countWords(string str){
     str = ' ' + str + ' ';
     int pr1 = 0, pr2;
+    int kol = 0;
     for(int i = 0; i < itc_len(str); i++){
         if (str[i] == ' '){
             pr2 = i;
             string sl;
-            sl = pol(str, pr1, pr2);
+            sl = pol(str, pr1 + 1, pr2 - 1);
             if (slo(sl))
                 kol++;
             pr1 = pr2;
         }
     }
+    return kol;
 }
 
 
